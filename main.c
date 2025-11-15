60c1393c37a17aaedd338ee3078776bb8afa3dc0
kei watanabe <kei.ashley.watanabe@gmail.com>
2025-11-15 21:48:44 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+//WATANABE, KEI ASHLEY L. DIT 1-2
+
+#include <stdio.h>
+
+int main() {
+    int age, hasAnaktID, score, isSpecies;
+
+    printf("***********************\n");
+    printf("Welcome to Alien Stage!\n");
+    printf("***********************\n\n");
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+
+    printf("Do you have an ANAKT GARDEN identification card? (1 = Yes, 0 = No): ");
+    scanf("%d", &hasAnaktID);
+
+    printf("Please enter your score: ");
+    scanf("%d", &score);
+
+    printf("What is your species? (1 = Human, 0 = Alien): ");
+    scanf("%d", &isSpecies);
+
+
+    if (age > 15 || hasAnaktID == 1) {
+        printf("\nYou are allowed to enter the Alien Stage comptetion.\n");
+    } else {
+        printf("\nYou are NOT allowed to enter the Alien Stage comptetion\n");
+    }
+
+
+    if (score >= 90 && isSpecies == 1) {
+        printf("CONGRATULATIONS! You are also qualified to advance in the semi-finals.\n");
+    } else {
+        printf("Sorry. You can't advance and must proceed normally.\n");
+    }
+
+
+    if (!isSpecies) {
+        printf("Only humans can participate in the competetion.\n");
+    } else {
+        printf("Welcome, human!\n");
+    }
+
+    return 0;
+}
